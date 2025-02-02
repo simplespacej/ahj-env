94bdec741bc3ed9122f920baa2ab7e25cdaf3fc0
simplespacej <128588608+simplespacej@users.noreply.github.com>
2025-02-02 11:37:41 +0000

    Deploying to gh-pages from @ simplespacej/ahj-env@0a0a7dc3d0617be940b137a1b956cf96e36b33cc 🚀

---
+++ main.js
@@ -0,0 +1,21 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+;// ./src/js/app.js
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = "variable";
+
+// for demonstration purpose only
+function demo(value) {
+  return `Demo: ${value}`;
+}
+console.log("app.js included");
+;// ./src/index.js
+
+
+
+// TODO: write your code in app.js
+/******/ })()
+;
